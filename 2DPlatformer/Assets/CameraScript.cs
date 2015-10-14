51e463b398c64845211c97ffc14be02a25fc91ea
Jack Storm <jacob.storm@mymail.champlain.edu>
2015-10-14 12:11:12 -0400

    Template for 5 levels Added a rudimentary AI Plane that will attempt to follow you

---
+++ 2DPlatformer/Assets/CameraScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CameraScript : MonoBehaviour {
+
+    public Camera cam;
+    public Transform target;
+    public GameObject camTexture;
+
+	// Use this for initialization
+	void Start () {
+        if(!cam)
+            cam = GetComponent<Camera>();
+        //cam.enabled = false;
+        //border.enabled = false;
+
+        DontDestroyOnLoad(this.transform.parent.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.position + new Vector3(0, 0, -34);
+
+
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            cam.enabled = !cam.enabled;
+            camTexture.SetActive(cam.enabled);
+        }
+	}
+}
---
+++ 3DAdventure/Assets/Scripts/AI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+
+	// Use this for initialization
+    Rigidbody rb;
+    public float speed = 50;
+    public float maxRotDegrees = 60;
+    float yRot;
+    public float yaw;
+    float xRot;
+    float zRot;
+
+    public Transform target;
+	void Start () {
+        if (!target)
+            target = GameObject.Find("PlayerOne").transform;
+        if (!rb)
+            rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 direction = target.position - transform.position;
+        //Vector3.Lerp(transform.forward, direction, Time.deltaTime);
+        
+        transform.rotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.LookRotation(direction), maxRotDegrees * Time.deltaTime);
+       
+	}
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * speed);
+
+    }
+}
