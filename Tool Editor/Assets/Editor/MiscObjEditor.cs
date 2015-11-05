49baa0c1d0de2907aa46ac9ad6d3e60d721ec523
Jack <jsto27@gmail.com>
2015-11-04 19:48:35 -0500

    Added things

---
+++ Tool Editor/Assets/Editor/MiscObjEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MiscObjGenerator))]
+public class MiscObjEditor : Editor 
+{
+    /*
+    public override void OnInspectorGUI()
+    {
+    }
+    */
+}
