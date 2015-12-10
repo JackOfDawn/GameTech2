37ec21b21ea2f41b85ecaf5362548f995397451b
Jack Storm <jacob.storm@mymail.champlain.edu>
2015-12-10 12:33:04 -0500

    player can now drop bombs

--- bombbay/Assets/BombScript.cs
+++ bombbay/Assets/BombScript.cs
@@ -3,13 +3,42 @@ using System.Collections;
 
 public class BombScript : MonoBehaviour {
 
+    Rigidbody rb;
+    bool collided = false;
+    Animator explosionAnim;
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+        rb.velocity = Vector3.zero;
+
+        explosionAnim = GetComponentInChildren<Animator>();
 	}
+
+    void Update()
+    {
+        if(!rb.isKinematic)
+            transform.LookAt( transform.position + rb.velocity);
+    }
+
+    public void Release(Vector3 velocity)
+    {
+        rb.isKinematic = false;
+        rb.velocity = velocity;
+        transform.parent = null;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(!collided)
+        {
+            explosionAnim.SetTrigger("EXPLODE");
+            rb.isKinematic = true;
+        }
+        collided = true;
+
+
+    }
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
